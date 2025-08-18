61ce49824cc270188c00db3b1524cf7b06613bbd
strrev-dev <deepanmaurya699@gmail.com>
2025-08-18 21:37:08 +0530

    sqare pattern

---
+++ square_pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n,m;
+ cin>> n;
+ cin>> m;
+    for( int i =1; i <=n; i++){
+        for (int j = 1;  j <= m;j++)
+        {
+            cout<<j <<" ";
+        }
+   cout<< endl;
+        
+    }
+    return 0;
+}
+
