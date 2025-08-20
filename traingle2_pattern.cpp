a9495a8cd8ddff42863d0f1ece9c5915f7490a7a
strrev-dev <deepanmaurya699@gmail.com>
2025-08-20 13:05:42 +0530

    traingle pattern second

---
+++ traingle2_pattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n=5;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < i+1; j++)
+        {
+            cout<< j;
+
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
